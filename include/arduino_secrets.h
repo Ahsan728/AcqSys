74d076fc70fd23ebef8527ef57bd9d524df34213
AhsanSuny <34878134+Ahsan728@users.noreply.github.com>
2023-06-22 20:50:28 +0200

    Add files via upload

---
+++ include/arduino_secrets.h
@@ -0,0 +1,11 @@
+/*
+#define SECRET_SSID "Numericable-e140"
+#define SECRET_PASS "qvn3qjftkq37"
+*/
+
+/*
+#define SECRET_SSID "AHSAN"
+#define SECRET_PASS "ahsan123"
+*/
+#define SECRET_SSID "NUMERICABLE-76E8"
+#define SECRET_PASS "894EB66893"
